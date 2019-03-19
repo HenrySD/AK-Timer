b2247991126920cdec5ac5c2a490d8dd4edf4428
Lopez.Henry <henryderaslopez@yahoo.com>
2019-03-18 23:24:45 -0500

    inicio

--- login.php
+++ login.php
@@ -5,12 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Inicio de Seccion</title>
 
     <!--CDN boostrap-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="carpetas/css/estilo.css">
 
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="carpetas/js/jquery.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js">
+</script>
+<script src="particles.js"></script>
+<script src="particulas.js"></script>
 
 
 
@@ -18,6 +25,7 @@
 
 <body>
 
+<div id="particles-js"></div>
 
     <form action="singup.php" method="post" class="login text-center ">
         <div class="form-group ">
@@ -32,23 +40,9 @@
             <div class="col">
                 <button type="submit" class="btn btn-primary btn-block" id="ingresar">Ingresar</button>
             </div>
-            <div class="col">
-                <button type="submit" class="btn btn-danger btn-block" id="ingresar">Registrar</button>
-            </div>
+            
         </div>
     </form>
+    
 </body>
-
-
-
-
-
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js">
-</script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js">
-</script>
-
-
 </html>
